e18e8155e041a2b51dbbda8b022946b175f0fafc
HyungjinLee <2014104136@khu.ac.kr>
2020-08-11 11:30:27 +0900

    README edited

--- model.py
+++ model.py
@@ -22,7 +22,7 @@ def select_model(name):
         print('selected (fine-tuning) inception model')
         return inception_v3
     elif name == 'model2' :
-        return levi_hassner_2
+        return levi_hassner2
     elif name == 'bn':
         print('selected batch norm model')
         return levi_hassner_bn
@@ -189,6 +189,43 @@ def levi_hassner(nlabels, images, pkeep, is_training):
                 drop2 = tf.nn.dropout(full2, pkeep, name='drop2')
 
     with tf.variable_scope('output') as scope:
+        
+        weights = tf.Variable(tf.random_normal([512, nlabels], mean=0.0, stddev=0.01), name='weights')
+        biases = tf.Variable(tf.constant(0.0, shape=[nlabels], dtype=tf.float32), name='biases')
+        output = tf.add(tf.matmul(drop2, weights), biases, name=scope.name)
+    return output
+
+def levi_hassner2(nlabels, images, pkeep, is_training):
+
+    weight_decay = 0.0005
+    weights_regularizer = tf.contrib.layers.l2_regularizer(weight_decay)
+    with tf.variable_scope("LeviHassner2", "LeviHassner2", [images],reuse=tf.AUTO_REUSE) as scope:
+
+        with tf.contrib.slim.arg_scope(
+                [convolution2d, fully_connected],
+                weights_regularizer=weights_regularizer,
+                biases_initializer=tf.constant_initializer(1.),
+                weights_initializer=tf.random_normal_initializer(stddev=0.005),
+                trainable=True):
+            with tf.contrib.slim.arg_scope(
+                    [convolution2d],
+                    weights_initializer=tf.random_normal_initializer(stddev=0.01)):
+
+                conv1 = convolution2d(images, 96, [7,7], [4, 4], padding='VALID', biases_initializer=tf.constant_initializer(0.), scope='conv1')
+                pool1 = max_pool2d(conv1, 3, 2, padding='VALID', scope='pool1')
+                norm1 = tf.nn.local_response_normalization(pool1, 5, alpha=0.0001, beta=0.75, name='norm1')
+                conv2 = convolution2d(norm1, 256, [5, 5], [1, 1], padding='SAME', scope='conv2') 
+                pool2 = max_pool2d(conv2, 3, 2, padding='VALID', scope='pool2')
+                norm2 = tf.nn.local_response_normalization(pool2, 5, alpha=0.0001, beta=0.75, name='norm2')
+                conv3 = convolution2d(norm2, 384, [3, 3], [1, 1], biases_initializer=tf.constant_initializer(0.), padding='SAME', scope='conv3')
+                pool3 = max_pool2d(conv3, 3, 2, padding='VALID', scope='pool3')
+                flat = tf.reshape(pool3, [-1, 384*6*6], name='reshape')
+                full1 = fully_connected(flat, 512, scope='full1')
+                drop1 = tf.nn.dropout(full1, pkeep, name='drop1')
+                full2 = fully_connected(drop1, 512, scope='full2')
+                drop2 = tf.nn.dropout(full2, pkeep, name='drop2')
+
+    with tf.variable_scope('output2') as scope:
         
         weights = tf.Variable(tf.random_normal([512, nlabels], mean=0.0, stddev=0.01), name='weights')
         biases = tf.Variable(tf.constant(0.0, shape=[nlabels], dtype=tf.float32), name='biases')
